1dcac97bde3777bc5c0bfaec500e2cc9a9fc89b1
joshuabecker91 <joshua.becker91@gmail.com>
2022-12-08 10:21:51 -0800

    adding notes on how to use

--- audio_mic_transcribe.py
+++ audio_mic_transcribe.py
@@ -1,6 +1,14 @@
+# Notes -------------------------------------------------------------------------
 # must install the following:
 # pip install speechrecognition
 # pip install pyttsx3 --user
+# -------------------------------------------------------------------------------
+# In terminal run: py audio_mic_transcribe.py 
+# If transcribed.txt does not exist it will create the file
+# Speak into your mic and it will interpret what you said and print to text file
+# Listens for a pause to write a new line
+# You can edit the text file manually and save at anytime
+# -------------------------------------------------------------------------------
 
 import speech_recognition
 import pyttsx3
